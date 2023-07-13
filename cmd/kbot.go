7ea64e65467da39d7b9fed0a7659861af0795748
Vitalii Prutskyi <48723966+vitalibit@users.noreply.github.com>
2023-07-13 04:12:38 +0300

    Update kbot.go

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -30,7 +30,7 @@ to quickly create a Cobra application.`,
 
 		fmt.Printf("kbot %s started", appVersion)
 
-		tokenBytes, err := ioutil.ReadFile("/etc/app/secrets")
+		tokenBytes, err := ioutil.ReadFile("/etc/app/secret/kbot")
 		if err != nil {
 			log.Println("Failed to read token file:", err)
 			return
